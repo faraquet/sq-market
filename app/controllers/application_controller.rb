ba3d2a69fb408c14697e17526e40184c90dc5c62
faraquet <yaidzo@yandex.ru>
2016-11-12 19:46:30 +0500

    Create check request format method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+  before_action :check_request_format
+
+  private
+
+  def record_not_found
+    render status: 404, json: { error: "Record not found" }
+  end
+
+  def check_request_format
+    (render status: 406, json: { error: "Format should be JSON" }) if request.format != :json
+  end
+  
 end
